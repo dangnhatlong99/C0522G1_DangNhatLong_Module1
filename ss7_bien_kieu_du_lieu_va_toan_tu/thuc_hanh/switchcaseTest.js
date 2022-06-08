f52e612dcb77ee3fc418b1367c3e645ec4b3344a
Long <dangnhatlong2309@gmail.com>
2022-06-08 22:40:55 +0700

    update 8/6

---
+++ ss7_bien_kieu_du_lieu_va_toan_tu/thuc_hanh/switchcaseTest.js
@@ -0,0 +1,22 @@
+const a = 1;
+
+// switch (a) {
+//     case 1: alert("so 1");
+//         // break;
+//     case 2: alert("so 2");
+//         // break;
+//     case 3: alert("so 3");
+//     break;
+//     default: alert("so con lai")
+// }
+
+//if - else;
+if (a === 1){
+    alert("so 1")
+}else if (a === 2){
+    alert("so 2")
+}else if (a === 3){
+    alert("so 3")
+}else if (a === 4){
+    alert("so 3")
+}else alert("so con lai")
